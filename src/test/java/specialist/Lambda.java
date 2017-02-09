f734fc29031faa80c52f786dd1ce0ac73917a710
SerdukovAA <serdukovaa@autoins.ru>
2017-02-09 12:36:24 +0300

    specialist second tutorial

--- src/test/java/specialist/Lambda.java
+++ src/test/java/specialist/Lambda.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by serdukovaa on 09.02.2017.
@@ -42,10 +43,11 @@ public class Lambda {
         }
     }
 
-    @FunctionalInterface
+    @FunctionalInterface // один метод!
     interface ElecticifyConsumer{
        void electrycityOn();
-   }
+       //void electrycityOn2();
+    }
 
     @Test
     public void firstTutorial(){
@@ -62,13 +64,61 @@ public class Lambda {
             }
         });
 
-        //And now Magic!!!
+        //And now is Magic Time!!!
         //Ta-Da!!))
-        switcher.consumers.add(()-> System.out.println("Lambda expression"));
+        switcher.consumers.add(()-> System.out.println("Lambda expression"));  // - крыглые скобки можно опускать когда ровно 1 параметр! ни одного или более 1 то обязательно круглые скобки.
+
+        switcher.consumers.add(Lambda::fire);
+
+        switcher.consumers.stream().forEach(ElecticifyConsumer::electrycityOn);
+
 
         switcher.switchOn();
 
     }
 
+    static void fire(String name){
+        System.out.println("пожарище "+name);
+    }
 
+    static void fire(){
+        System.out.println("пожарище");
+    }
+
+
+
+    class Person {
+
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void print(){
+            System.out.println(name +" - "+age);
+        }
+
+    }
+
+    @Test
+    public void streamTutorial(){
+      List<Person> persons = new ArrayList<>();
+      persons.add(new Person("Гена",32));
+      persons.add(new Person("Саша",27));
+      persons.add(new Person("Ваня",45));
+      persons.add(new Person("Боня",22));
+      persons.add(new Person("Катя",19));
+
+        persons.stream().forEach(Person::print);
+
+
+        Optional<Person> ps = persons.stream().findAny();
+
+        ps.get().print();
+    }
 }
+
+
